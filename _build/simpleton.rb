8ce4f44ed60116ad8a6f475ac759e317c0715fe6
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2010-01-09 15:51:53 +0000

    Extract Simpleton into a self-contained Sinatra app
     - Option to redirect indexes (302) to trailing slash
     - Index file is now configurable

--- _build/simpleton.rb
+++ _build/simpleton.rb
@@ -1,13 +1,28 @@
-require 'rubygems'
-require 'sinatra'
 
-# @TODO: Make SSI logic accept single or double quotes
+# @TODO: Make load path configurable (or fetched from sinatra)
 # @TODO: Generalise unsupported patterns to match all SSI directives
+
+# Mixin the ability to read 
+# a file from a filesystem 
+# path
+module FileReader
+  def read_file(path)
+    puts path
+    file = ""
+    File.open(path, "r") do |infile|
+      while (line = infile.gets)
+        file << line
+      end
+    end
+    file
+  end
+end
 
 
 # Rack middleware to parse SSIs
 # in the response body
 class SSIParser  
+  include FileReader
   def initialize(app)  
     @app = app  
     
@@ -48,10 +63,10 @@ class SSIParser
     return matches_in_doc
   end
   
-  def load_paths( paths )
+  def load_paths( relative_paths, path_base=File.dirname(__FILE__) + "/../_site" )    
     all = []
-    paths.each do |p|
-      path = '/Users/andrew/Projects/Work/andrewn.github.com/_site' + p.first
+    relative_paths.each do |p|
+      path = path_base + p.first
       c = read_file( path )
       all << {
         :path => p,
@@ -63,42 +78,67 @@ class SSIParser
 end
 
 
-# Sinatra app
-#
+require 'rubygems'
+require 'sinatra/base'
 
-use SSIParser
+# Sinatra app to do basic static 
+# file serving but with understanding
+# of index files and index redirection
+# i.e.  /       -> /index.html [Loads]
+#       /about  -> /about/index.html [Loads]
+#       /about  -> /about/ [Redirect] [Optional]
+#       /about/ -> /about/index.html [Loads]
+class Simpleton < Sinatra::Default
+  
+  include FileReader
+  
+  use SSIParser
 
-base_path = File.dirname(__FILE__) + "/../_site"
-puts base_path
+  configure do 
+    disable :redirect_to_trailing_slash
+    set     :index_file, "index.html"
+  end
 
-set :public, "_site"
+  enable :redirect_to_trailing_slash
+  set :base_path, File.dirname(__FILE__) + "/../_site"
+  set :public, File.dirname(__FILE__) + "/../_site"
 
-get '/' do
-  file_path = base_path + "/index.html"
-  file = read_file( file_path )
-end
+  get '/' do
+    puts 'index'
+    file_path = options.base_path + "/#{options.index_file}"
+    file = read_file( file_path )
+  end
 
-get /(\/[\w]+\/)/ do
-  file_path = base_path + params[:captures].to_s + "/index.html"
-  read_file( file_path )
-end
+  get /(\/[\w]+\/)\z/ do
+    puts 'trailing slash'
+    url_path = params[:captures].to_s + "/#{options.index_file}"    
+    file_path = options.base_path + url_path
+    read_file( file_path )
+  end
 
-get '*' do
-  file_path = base_path + params[:splat].to_s
-  is_dir = File.directory? file_path
-  pass unless is_dir
-  file_path = file_path + "/index.html"
-  read_file(file_path)
-end
-
-private 
-def read_file(path)
-  puts path
-  file = ""
-  File.open(path, "r") do |infile|
-    while (line = infile.gets)
-      file << line
+  get '*' do
+    puts "Splat (no trailing slash)"
+    
+    # The root-relative url path
+    url_path = params[:splat].to_s
+        
+    file_path = options.base_path + url_path
+    is_dir = File.directory? file_path
+    
+    puts "Passing to #{options.public}" unless is_dir
+    pass unless is_dir
+    
+    # Redirect /about -> /about/
+    puts "** #{options.redirect_to_trailing_slash}"
+    if options.redirect_to_trailing_slash
+      redirect url_path + "/"
+    else
+    # Silently read index 
+      file_path = file_path + "/#{options.index_file}"
+      read_file(file_path)
     end
   end
-  file
+
 end
+
+Simpleton.run!
