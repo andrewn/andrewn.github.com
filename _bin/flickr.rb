4a28425a2ce46fa900e7ae8d49ab8bc67ee9f383
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2010-02-28 19:56:35 +0000

    change flickr script to filter out images tagged as blank. Also, allow flexibility to return more images in list. Have set to 1 to start

--- _bin/flickr.rb
+++ _bin/flickr.rb
@@ -5,38 +5,49 @@ require 'open-uri'
 class Processor
 
   attr_reader :html
-
-  @@debug = false
-  @@no_title_text = "(No title)"
+  
+  @@num_items       = 1
+  @@ignore          = /blank/i
+  @@debug           = false
+  @@no_title_text   = "(No title)"
   @html
   
   def initialize    
-    limit = 1
-    html_template = '<li><a href="{link}"><img src="{url}" /></a></li>'
+    limit = @@num_items
+    html_template = <<-doc
+      <li>
+        <a href="{link}">
+          <img src="{url}" />
+        </a>
+      </li>
+    doc
     rss_url = "http://api.flickr.com/services/feeds/photoset.gne?set=72157622997668729&nsid=97682155@N00&lang=en-us&format=rss_200"
     proxy = nil
     
     doc = open(rss_url, :proxy => proxy) { |f| Hpricot::XML(f) }
-    children = doc.search("//item").slice(0 , limit)
+    children = doc.search("//item") #.slice(0 , limit)
 
-    @html = create_html_from_rss_items( children, html_template )
+    @html = create_html_from_rss_items( children, html_template, limit )
   end
 
-  def create_html_from_rss_items( items, template )
+  def create_html_from_rss_items( items, template, num_to_return = 1 )
     
     puts "There are #{items.length} photos" if @@debug
     
-    output = ""
+    ignore = @@ignore
+    
+    output = []
     items.each do | item |
       title = item.at( "title" ).inner_html
       link  = item.at( "link" ).inner_html  
       url   = item.at( "media:thumbnail" ).attributes["url"]
+      tags  = item.at( "media:category").inner_html
       
-      puts "Title: #{title}, link: #{link}, url: #{url}" if @@debug
+      puts "Title: #{title}, link: #{link}, url: #{url}, tags: #{tags}" if @@debug
       
-      output << replace_in_template_string( template, { :title => title, :link => link, :url => url } )
+      output << replace_in_template_string( template, { :title => title, :link => link, :url => url } ) unless ( ignore.nil? ? false : tags.match( ignore ) )
     end
-    return output
+    return output.slice(0, num_to_return).join(" ")
   end
   
   def replace_in_template_string( str="", hash={} ) 
@@ -56,4 +67,4 @@ class Processor
   end
 end
 
-puts "<ol>" + Processor.new.html + "</ol>"
+puts "<ol>\n" + Processor.new.html + "\n</ol>"
