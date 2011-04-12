de2acb174487b3d6a0d0cc918d75c9d9781e7012
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-04-13 00:54:26 +0800

    error

--- frontend.rb
+++ frontend.rb
@@ -86,7 +86,7 @@ module AndrewNicolaou
       # set domain of incoming request
       @host = "#{request.scheme}://#{request.host_with_port}"
       @host = ""
-      warn "App host is: " + @host
+      #warn "App host is: " + @host
     end
 
     
