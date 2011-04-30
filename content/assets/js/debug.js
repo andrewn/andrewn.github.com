d7e75a0eece6bfef9d9038e0964572cd86a9eef4
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-04-30 10:29:02 +0100

    Change debug class name to match CSS

--- content/assets/js/debug.js
+++ content/assets/js/debug.js
@@ -1,13 +1,14 @@
 (function() {
     
-    var DEBUG_MODE = 'debug';
+    var DEBUG_MODE  = 'debug',
+        DEBUG_CLASS = 'debug';
 
     function isDebug() {
         return (new RegExp(DEBUG_MODE)).test(location.search);
     }
 
     function enableGrid() {
-        document.body.className += 'grid';
+        document.body.className += DEBUG_CLASS;
     }
 
     function init() {
