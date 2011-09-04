0679b1377746622392b1c2db43a34d3c341e2b76
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-09-04 17:51:14 +0100

    output filepath logging

--- layout/views/post_list.rb
+++ layout/views/post_list.rb
@@ -1,17 +1,18 @@
-#require 'layout/views/post'
-#puts "Loading static postlist class"
-#module AndrewNicolaou
-#  class Frontend
-#    module Views      
-#      class PostList < Layout
-#          def posts
-#            puts "PostList #{@posts.length}"
-#            @posts.map do |post|
-#              puts "List #{post}"
-#              Post.new(post)
-#            end
-#          end
-#      end
-#    end
-#  end
-#end
+require 'layout/views/post'
+puts "File path " + File.dirname(__FILE__)
+puts "Loading static postlist class"
+module AndrewNicolaou
+  class Frontend
+    module Views      
+      class PostList < Layout
+          def posts
+            puts "PostList #{@posts.length}"
+            @posts.map do |post|
+              puts "List #{post}"
+              Post.new(post)
+            end
+          end
+      end
+    end
+  end
+end
