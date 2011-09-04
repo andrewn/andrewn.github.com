2d1ca64c1d1e1a7f6415e70487f8988ff06c406a
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-09-04 16:35:09 +0100

    list log

--- layout/views/post_list.rb
+++ layout/views/post_list.rb
@@ -4,6 +4,7 @@ module AndrewNicolaou
     module Views      
       class PostList < Layout
           def posts
+            puts "PostList #{@posts.length}"
             @posts.map do |post|
               puts "List #{post}"
               Post.new(post)
